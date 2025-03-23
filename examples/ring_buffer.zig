442a466fbe3470c1a7741f36f0910651d4d6a325
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-03-22 20:25:44 -0500

    Add ring buffer example and documentation
    Updates the build.zig to build examples and updates documentation to
    follow the pattern that I want to show users of this library how they
    should use the data structures/tools.

---
+++ examples/ring_buffer.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+const stdx = @import("stdx");
+const RingBuffer = stdx.RingBuffer;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    // Create a basic ring buffer
+    var ring_buffer = try RingBuffer(u64).init(allocator, 100);
+    defer ring_buffer.deinit();
+
+    // Enqueue a single item
+    const first_value: u64 = 10;
+    try ring_buffer.enqueue(first_value);
+
+    assert(ring_buffer.count == 1);
+
+    // Enqueue many items
+    const values: [3]u64 = [_]u64{9999} ** 3;
+    const enqueued_count = ring_buffer.enqueueMany(&values);
+
+    assert(enqueued_count == values.len);
+
+    // fill the remaining capacity of the ring buffer
+    ring_buffer.fill(4321);
+
+    assert(ring_buffer.available() == 0);
+
+    // dequeue a single item from the ring buffer
+    const v = ring_buffer.dequeue();
+    assert(first_value == v);
+
+    // dequeue many items from the queue
+    var out: [2]u64 = [_]u64{0} ** 2;
+    const dequeued_items_count = ring_buffer.dequeueMany(&out);
+
+    assert(dequeued_items_count == out.len);
+
+    // reset the ring buffer
+    ring_buffer.reset();
+
+    assert(ring_buffer.isEmpty());
+}
