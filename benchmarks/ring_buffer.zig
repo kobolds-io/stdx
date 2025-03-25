f434f7f489ca0123097f00123b4f00a5b5e92e79
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-03-25 17:14:51 -0500

    Fix RingBuffer benchmark wrong function names
    The BenchmarkRingBufferDequeue & BenchmarkRingBufferDequeueMany were
    named incorrectly leading to incorrect results output. Additionally,
    added a fix to use `hooks` to reset ring buffers to known states between
    each iteration making the output much more realistic to what we would
    see IRL.

--- benchmarks/ring_buffer.zig
+++ benchmarks/ring_buffer.zig
@@ -11,19 +11,19 @@ const RingBuffer = stdx.RingBuffer;
 const BenchmarkRingBufferEnqueue = struct {
     const Self = @This();
 
-    data_list: *std.ArrayList(usize),
+    list: *std.ArrayList(usize),
     ring_buffer: *RingBuffer(usize),
 
-    fn new(data_list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
+    fn new(list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
         return .{
-            .data_list = data_list,
+            .list = list,
             .ring_buffer = ring_buffer,
         };
     }
 
     pub fn run(self: Self, _: std.mem.Allocator) void {
-        // enqueue every data point in the data_list into the ring buffer
-        for (self.data_list.items) |data| {
+        // enqueue every data point in the list into the ring buffer
+        for (self.list.items) |data| {
             self.ring_buffer.enqueue(data) catch unreachable;
         }
 
@@ -35,88 +35,97 @@ const BenchmarkRingBufferEnqueue = struct {
 const BenchmarkRingBufferEnqueueMany = struct {
     const Self = @This();
 
-    data_list: *std.ArrayList(usize),
+    list: *std.ArrayList(usize),
     ring_buffer: *RingBuffer(usize),
 
-    fn new(data_list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
+    fn new(list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
         return .{
-            .data_list = data_list,
+            .list = list,
             .ring_buffer = ring_buffer,
         };
     }
 
     pub fn run(self: Self, _: std.mem.Allocator) void {
-        // enqueue every data point in the data_list into the ring buffer
-        const n = self.ring_buffer.enqueueMany(self.data_list.items);
+        // enqueue every data point in the list into the ring buffer
+        const n = self.ring_buffer.enqueueMany(self.list.items);
 
-        assert(n == self.data_list.items.len);
+        assert(n == self.list.items.len);
 
         // drop ALL references immediately
         self.ring_buffer.reset();
     }
 };
 
-const BenchmarkRingBufferDequeueMany = struct {
+const BenchmarkRingBufferDequeue = struct {
     const Self = @This();
 
-    data_list: *std.ArrayList(usize),
+    list: *std.ArrayList(usize),
     ring_buffer: *RingBuffer(usize),
 
-    fn new(data_list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
+    fn new(list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
         return .{
-            .data_list = data_list,
+            .list = list,
             .ring_buffer = ring_buffer,
         };
     }
 
     pub fn run(self: Self, _: std.mem.Allocator) void {
-        // NOTE: we are gonna do some hacking here. This is simulating that the ring buffer has some
-        // data inside of it but it simply does not. This operation should be almost instantaneos
-        self.ring_buffer.head = 0;
-        self.ring_buffer.tail = self.data_list.items.len;
-        self.ring_buffer.count = @intCast(self.data_list.items.len);
-        // this is a sanity check
-        assert(self.ring_buffer.count <= self.ring_buffer.capacity);
-
-        // actual testk
         while (self.ring_buffer.dequeue()) |_| {}
         assert(self.ring_buffer.isEmpty());
     }
 };
 
-const BenchmarkRingBufferDequeue = struct {
+const BenchmarkRingBufferDequeueMany = struct {
     const Self = @This();
 
-    data_list: *std.ArrayList(usize),
+    list: *std.ArrayList(usize),
     ring_buffer: *RingBuffer(usize),
 
-    fn new(data_list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
+    fn new(list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize)) Self {
         return .{
-            .data_list = data_list,
+            .list = list,
             .ring_buffer = ring_buffer,
         };
     }
 
     pub fn run(self: Self, _: std.mem.Allocator) void {
-        // NOTE: we are gonna do some hacking here. This is simulating that the ring buffer has some
-        // data inside of it but it simply does not. This operation should be almost instantaneos
-        self.ring_buffer.head = 0;
-        self.ring_buffer.tail = self.data_list.items.len;
-        self.ring_buffer.count = @intCast(self.data_list.items.len);
-        // this is a sanity check
-        assert(self.ring_buffer.count <= self.ring_buffer.capacity);
-
-        // actual test
-        const n = self.ring_buffer.dequeueMany(self.data_list.items);
+        const n = self.ring_buffer.dequeueMany(self.list.items);
 
         assert(self.ring_buffer.isEmpty());
-        assert(n == self.data_list.items.len);
+        assert(n == self.list.items.len);
     }
 };
 
-test "RingBuffer benchmarks" {
-    const allocator = testing.allocator;
+var ring_buffer_enqueue: RingBuffer(usize) = undefined;
+var ring_buffer_enqueueMany: RingBuffer(usize) = undefined;
+var ring_buffer_dequeue: RingBuffer(usize) = undefined;
+var ring_buffer_dequeueMany: RingBuffer(usize) = undefined;
+var data_list: std.ArrayList(usize) = undefined;
+const allocator = testing.allocator;
 
+fn beforeEachDequeue() void {
+    // NOTE: we are gonna do some hacking here. This is simulating that the ring buffer has some
+    // data inside of it but it simply does not. This operation should be almost instantaneos
+    ring_buffer_dequeue.head = 0;
+    ring_buffer_dequeue.tail = data_list.items.len;
+    ring_buffer_dequeue.count = @intCast(data_list.items.len);
+
+    // this is a sanity check
+    assert(ring_buffer_dequeue.count <= ring_buffer_dequeue.capacity);
+}
+
+fn beforeEachDequeueMany() void {
+    // NOTE: we are gonna do some hacking here. This is simulating that the ring buffer has some
+    // data inside of it but it simply does not. This operation should be almost instantaneos
+    ring_buffer_dequeueMany.head = 0;
+    ring_buffer_dequeueMany.tail = data_list.items.len;
+    ring_buffer_dequeueMany.count = @intCast(data_list.items.len);
+
+    // this is a sanity check
+    assert(ring_buffer_dequeueMany.count <= ring_buffer_dequeueMany.capacity);
+}
+
+test "RingBuffer benchmarks" {
     var bench = zbench.Benchmark.init(
         std.testing.allocator,
         .{ .iterations = constants.benchmark_max_iterations },
@@ -124,10 +133,10 @@ test "RingBuffer benchmarks" {
     defer bench.deinit();
 
     // Create a list of `n` length that will be used/reused by each benchmarking test
-    var data_list = std.ArrayList(usize).initCapacity(
+    data_list = try std.ArrayList(usize).initCapacity(
         allocator,
         constants.benchmark_max_queue_data_list,
-    ) catch unreachable;
+    );
     defer data_list.deinit();
 
     // fill the data list with items
@@ -135,16 +144,17 @@ test "RingBuffer benchmarks" {
         data_list.appendAssumeCapacity(@intCast(i));
     }
 
-    var ring_buffer_enqueue = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
+    // Initialize all ring buffers used in benchmarks
+    ring_buffer_enqueue = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
     defer ring_buffer_enqueue.deinit();
 
-    var ring_buffer_enqueueMany = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
+    ring_buffer_enqueueMany = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
     defer ring_buffer_enqueueMany.deinit();
 
-    var ring_buffer_dequeue = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
+    ring_buffer_dequeue = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
     defer ring_buffer_dequeue.deinit();
 
-    var ring_buffer_dequeueMany = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
+    ring_buffer_dequeueMany = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
     defer ring_buffer_dequeueMany.deinit();
 
     const ring_buffer_enqueue_title = try std.fmt.allocPrint(
@@ -172,10 +182,34 @@ test "RingBuffer benchmarks" {
     );
 
     // register all the benchmark tests
-    try bench.addParam(ring_buffer_enqueue_title, &BenchmarkRingBufferEnqueue.new(&data_list, &ring_buffer_enqueue), .{});
-    try bench.addParam(ring_buffer_enqueueMany_title, &BenchmarkRingBufferEnqueueMany.new(&data_list, &ring_buffer_enqueueMany), .{});
-    try bench.addParam(ring_buffer_dequeue_title, &BenchmarkRingBufferDequeue.new(&data_list, &ring_buffer_dequeue), .{});
-    try bench.addParam(ring_buffer_dequeueMany_title, &BenchmarkRingBufferDequeueMany.new(&data_list, &ring_buffer_dequeueMany), .{});
+    try bench.addParam(
+        ring_buffer_enqueue_title,
+        &BenchmarkRingBufferEnqueue.new(&data_list, &ring_buffer_enqueue),
+        .{},
+    );
+    try bench.addParam(
+        ring_buffer_enqueueMany_title,
+        &BenchmarkRingBufferEnqueueMany.new(&data_list, &ring_buffer_enqueueMany),
+        .{},
+    );
+    try bench.addParam(
+        ring_buffer_dequeue_title,
+        &BenchmarkRingBufferDequeue.new(&data_list, &ring_buffer_dequeue),
+        .{
+            .hooks = .{
+                .before_each = beforeEachDequeue,
+            },
+        },
+    );
+    try bench.addParam(
+        ring_buffer_dequeueMany_title,
+        &BenchmarkRingBufferDequeueMany.new(&data_list, &ring_buffer_dequeueMany),
+        .{
+            .hooks = .{
+                .before_each = beforeEachDequeueMany,
+            },
+        },
+    );
 
     // Write the results to stderr
     const stderr = std.io.getStdErr().writer();
