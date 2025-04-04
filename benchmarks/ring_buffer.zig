bd8ead483c3b3c2d127bf977792afe6284f61814
bgh <bengarishodge@pm.me>
2025-04-03 22:48:04 -0500

    Release v0.0.2 (#37)
    * reduce the number of benchmark iterations
    
    * add concatenate method to RingBuffer
    
    The method avoids reusing and recalculating some redundant math and
    mostly focuses on performance since this would be used fairly often
    
    * bump version number to 0.0.2

--- benchmarks/ring_buffer.zig
+++ benchmarks/ring_buffer.zig
@@ -96,10 +96,35 @@ const BenchmarkRingBufferDequeueMany = struct {
     }
 };
 
+const BenchmarkRingBufferConcatenate = struct {
+    const Self = @This();
+
+    list: *std.ArrayList(usize),
+    ring_buffer: *RingBuffer(usize),
+    ring_buffer_other: *RingBuffer(usize),
+
+    fn new(list: *std.ArrayList(usize), ring_buffer: *RingBuffer(usize), ring_buffer_other: *RingBuffer(usize)) Self {
+        return .{
+            .list = list,
+            .ring_buffer = ring_buffer,
+            .ring_buffer_other = ring_buffer_other,
+        };
+    }
+
+    pub fn run(self: Self, _: std.mem.Allocator) void {
+        const other_count = self.ring_buffer_other.count;
+        self.ring_buffer.concatenate(self.ring_buffer_other) catch @panic("could not execute benchmark");
+        assert(ring_buffer_concatenate.count == other_count);
+    }
+};
+
 var ring_buffer_enqueue: RingBuffer(usize) = undefined;
 var ring_buffer_enqueueMany: RingBuffer(usize) = undefined;
 var ring_buffer_dequeue: RingBuffer(usize) = undefined;
 var ring_buffer_dequeueMany: RingBuffer(usize) = undefined;
+var ring_buffer_concatenate: RingBuffer(usize) = undefined;
+var ring_buffer_concatenate_other: RingBuffer(usize) = undefined;
+
 var data_list: std.ArrayList(usize) = undefined;
 const allocator = testing.allocator;
 
@@ -123,6 +148,19 @@ fn beforeEachDequeueMany() void {
 
     // this is a sanity check
     assert(ring_buffer_dequeueMany.count <= ring_buffer_dequeueMany.capacity);
+}
+
+fn beforeEachConcatenate() void {
+    // NOTE: we are gonna do some hacking here. This is simulating that the ring buffer has some
+    // data inside of it but it simply does not. This operation should be almost instantaneos
+    ring_buffer_concatenate_other.head = 0;
+    ring_buffer_concatenate_other.tail = data_list.items.len;
+    ring_buffer_concatenate_other.count = @intCast(data_list.items.len);
+
+    ring_buffer_concatenate.reset();
+
+    // this is a sanity check
+    assert(ring_buffer_concatenate.capacity <= ring_buffer_concatenate_other.count);
 }
 
 test "RingBuffer benchmarks" {
@@ -157,29 +195,46 @@ test "RingBuffer benchmarks" {
     ring_buffer_dequeueMany = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
     defer ring_buffer_dequeueMany.deinit();
 
+    ring_buffer_concatenate = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
+    defer ring_buffer_concatenate.deinit();
+
+    ring_buffer_concatenate_other = try RingBuffer(usize).init(allocator, @intCast(data_list.items.len));
+    defer ring_buffer_concatenate_other.deinit();
+
     const ring_buffer_enqueue_title = try std.fmt.allocPrint(
         allocator,
         "enqueue {} items",
         .{constants.benchmark_max_queue_data_list},
     );
+    defer allocator.free(ring_buffer_enqueue_title);
 
     const ring_buffer_enqueueMany_title = try std.fmt.allocPrint(
         allocator,
         "enqueueMany {} items",
         .{constants.benchmark_max_queue_data_list},
     );
+    defer allocator.free(ring_buffer_enqueueMany_title);
 
     const ring_buffer_dequeue_title = try std.fmt.allocPrint(
         allocator,
         "dequeue {} items",
         .{constants.benchmark_max_queue_data_list},
     );
+    defer allocator.free(ring_buffer_dequeue_title);
 
     const ring_buffer_dequeueMany_title = try std.fmt.allocPrint(
         allocator,
         "dequeueMany {} items",
         .{constants.benchmark_max_queue_data_list},
     );
+    defer allocator.free(ring_buffer_dequeueMany_title);
+
+    const ring_buffer_concatenate_title = try std.fmt.allocPrint(
+        allocator,
+        "concatenate {} items",
+        .{constants.benchmark_max_queue_data_list},
+    );
+    defer allocator.free(ring_buffer_concatenate_title);
 
     // register all the benchmark tests
     try bench.addParam(
@@ -207,6 +262,19 @@ test "RingBuffer benchmarks" {
         .{
             .hooks = .{
                 .before_each = beforeEachDequeueMany,
+            },
+        },
+    );
+    try bench.addParam(
+        ring_buffer_concatenate_title,
+        &BenchmarkRingBufferConcatenate.new(
+            &data_list,
+            &ring_buffer_concatenate,
+            &ring_buffer_concatenate_other,
+        ),
+        .{
+            .hooks = .{
+                .before_each = beforeEachConcatenate,
             },
         },
     );
