fcb448ae420d75c59719b63d3b012ecd08482877
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-05-04 17:55:27 -0500

    add back_pressure mechanism for publishers to handle publish backpressure

--- examples/mpmc.zig
+++ examples/mpmc.zig
@@ -10,8 +10,12 @@ const log = std.log.scoped(.MPSCExample);
 
 // This is the type that will be processed
 const VALUE_TYPE: type = usize;
-const QUEUE_SIZE: usize = 10_000;
-const ITERATIONS = 1_000_000;
+const TOPIC_QUEUE_SIZE: usize = 50_000;
+const PUBLISHER_QUEUE_SIZE: usize = 5_000;
+const SUBSCRIBER_QUEUE_SIZE: usize = 5_000;
+const ITERATIONS = 10_000_000;
+const SUBSCRIBER_COUNT = 50;
+const PUBLISHER_COUNT = 1;
 
 pub fn Topic(comptime T: type) type {
     return struct {
@@ -25,7 +29,7 @@ pub fn Topic(comptime T: type) type {
             const queue = try allocator.create(RingBuffer(T));
             errdefer allocator.destroy(queue);
 
-            queue.* = try RingBuffer(T).init(allocator, QUEUE_SIZE);
+            queue.* = try RingBuffer(T).init(allocator, TOPIC_QUEUE_SIZE);
             errdefer queue.deinit();
 
             return Self{
@@ -52,12 +56,14 @@ pub fn Publisher(comptime T: type) type {
         queue: *RingBuffer(T),
         published_count: u128,
         mutex: std.Thread.Mutex,
+        close_channel: UnbufferedChannel(bool),
+        backpressure: std.ArrayList(VALUE_TYPE),
 
         pub fn init(allocator: std.mem.Allocator, id: usize, topic: *Topic(T)) !Self {
             const queue = try allocator.create(RingBuffer(T));
             errdefer allocator.destroy(queue);
 
-            queue.* = try RingBuffer(T).init(allocator, QUEUE_SIZE);
+            queue.* = try RingBuffer(T).init(allocator, PUBLISHER_QUEUE_SIZE);
             errdefer queue.deinit();
 
             return Self{
@@ -67,10 +73,13 @@ pub fn Publisher(comptime T: type) type {
                 .allocator = allocator,
                 .published_count = 0,
                 .mutex = .{},
+                .close_channel = UnbufferedChannel(bool).new(),
+                .backpressure = std.ArrayList(VALUE_TYPE).init(allocator),
             };
         }
 
         pub fn deinit(self: *Self) void {
+            self.backpressure.deinit();
             self.queue.deinit();
             self.allocator.destroy(self.queue);
         }
@@ -79,7 +88,12 @@ pub fn Publisher(comptime T: type) type {
             self.mutex.lock();
             defer self.mutex.unlock();
 
-            try self.queue.enqueue(value);
+            self.queue.enqueue(value) catch {
+                try self.backpressure.append(value);
+                // log.err("publisher: {} adding value to backpressure", .{self.id});
+                return;
+            };
+
             self.published_count += 1;
         }
 
@@ -93,15 +107,25 @@ pub fn Publisher(comptime T: type) type {
             self.mutex.lock();
             defer self.mutex.unlock();
 
+            if (self.backpressure.items.len > 0) {
+                const n = self.topic.queue.enqueueMany(self.backpressure.items);
+
+                std.mem.copyForwards(VALUE_TYPE, self.backpressure.items, self.backpressure.items[n..]);
+                self.backpressure.items.len -= n;
+                self.published_count += @intCast(n);
+                // log.err("self.backpressure.items.len {}, n {}", .{ self.backpressure.items.len, n });
+                // log.err("published count {}", .{self.published_count});
+            }
+
             self.topic.queue.concatenateAvailable(self.queue);
             // log.debug("publisher: {} self.queue.count {}", .{ self.id, self.queue.count });
         }
 
-        pub fn run(self: *Self, ready: *UnbufferedChannel(bool), close: *UnbufferedChannel(bool)) void {
+        pub fn run(self: *Self, ready: *UnbufferedChannel(bool)) void {
             ready.send(true);
             while (true) {
                 // check if we have received a signale to close the topic
-                const signal = close.timedReceive(0) catch false;
+                const signal = self.close_channel.timedReceive(0) catch false;
                 if (signal) {
                     log.info("signal received to stop publisher {}", .{self.id});
                     log.debug("publisher {}: published {} items", .{ self.id, self.published_count });
@@ -111,6 +135,10 @@ pub fn Publisher(comptime T: type) type {
                 self.tick() catch unreachable;
                 std.time.sleep(1 * std.time.ns_per_us);
             }
+        }
+
+        pub fn close(self: *Self) void {
+            self.close_channel.send(true);
         }
     };
 }
@@ -124,12 +152,13 @@ pub fn Subscriber(comptime T: type) type {
         id: usize,
         processed_count: u128,
         allocator: std.mem.Allocator,
+        close_channel: UnbufferedChannel(bool),
 
         pub fn init(allocator: std.mem.Allocator, id: usize, topic: *Topic(T)) !Self {
             const queue = try allocator.create(RingBuffer(T));
             errdefer allocator.destroy(queue);
 
-            queue.* = try RingBuffer(T).init(allocator, QUEUE_SIZE);
+            queue.* = try RingBuffer(T).init(allocator, SUBSCRIBER_QUEUE_SIZE);
             errdefer queue.deinit();
 
             return Self{
@@ -138,6 +167,7 @@ pub fn Subscriber(comptime T: type) type {
                 .id = id,
                 .processed_count = 0,
                 .queue = queue,
+                .close_channel = UnbufferedChannel(bool).new(),
             };
         }
 
@@ -156,11 +186,11 @@ pub fn Subscriber(comptime T: type) type {
             }
         }
 
-        pub fn run(self: *Self, ready: *UnbufferedChannel(bool), close: *UnbufferedChannel(bool)) void {
+        pub fn run(self: *Self, ready: *UnbufferedChannel(bool)) void {
             ready.send(true);
             while (true) {
                 // check if we have received a signale to close the topic
-                const signal = close.timedReceive(0) catch false;
+                const signal = self.close_channel.timedReceive(0) catch false;
                 if (signal) {
                     log.err("signal received to stop subscriber {}", .{self.id});
                     log.err("subscriber {}: processed {} items", .{ self.id, self.processed_count });
@@ -170,6 +200,10 @@ pub fn Subscriber(comptime T: type) type {
                 self.tick() catch unreachable;
                 std.time.sleep(1 * std.time.ns_per_us);
             }
+        }
+
+        pub fn close(self: *Self) void {
+            self.close_channel.send(true);
         }
     };
 }
@@ -182,94 +216,84 @@ pub fn main() !void {
     var topic = try Topic(VALUE_TYPE).init(allocator);
     defer topic.deinit();
 
-    var publisher_1 = try Publisher(VALUE_TYPE).init(allocator, 1, &topic);
-    defer publisher_1.deinit();
+    var publishers = std.ArrayList(*Publisher(VALUE_TYPE)).init(allocator);
+    defer publishers.deinit();
 
-    var publisher_2 = try Publisher(VALUE_TYPE).init(allocator, 2, &topic);
-    defer publisher_2.deinit();
+    var subscribers = std.ArrayList(*Subscriber(VALUE_TYPE)).init(allocator);
+    defer subscribers.deinit();
 
-    var subscriber_1 = try Subscriber(VALUE_TYPE).init(allocator, 1, &topic);
-    defer subscriber_1.deinit();
+    for (0..PUBLISHER_COUNT) |i| {
+        const publisher = try allocator.create(Publisher(VALUE_TYPE));
+        errdefer allocator.destroy(publisher);
 
-    var subscriber_2 = try Subscriber(VALUE_TYPE).init(allocator, 2, &topic);
-    defer subscriber_2.deinit();
+        publisher.* = try Publisher(VALUE_TYPE).init(allocator, i, &topic);
+        errdefer publisher.deinit();
 
-    var subscriber_3 = try Subscriber(VALUE_TYPE).init(allocator, 3, &topic);
-    defer subscriber_3.deinit();
+        try publishers.append(publisher);
+    }
 
-    var subscriber_1_close_channel = UnbufferedChannel(bool).new();
-    var subscriber_2_close_channel = UnbufferedChannel(bool).new();
-    var subscriber_3_close_channel = UnbufferedChannel(bool).new();
-    var publisher_1_close_channel = UnbufferedChannel(bool).new();
-    var publisher_2_close_channel = UnbufferedChannel(bool).new();
+    for (0..SUBSCRIBER_COUNT) |i| {
+        const subscriber = try allocator.create(Subscriber(VALUE_TYPE));
+        errdefer allocator.destroy(subscriber);
 
-    var subscriber_1_ready_channel = UnbufferedChannel(bool).new();
-    var subscriber_2_ready_channel = UnbufferedChannel(bool).new();
-    var subscriber_3_ready_channel = UnbufferedChannel(bool).new();
-    var publisher_1_ready_channel = UnbufferedChannel(bool).new();
-    var publisher_2_ready_channel = UnbufferedChannel(bool).new();
+        subscriber.* = try Subscriber(VALUE_TYPE).init(allocator, i, &topic);
+        errdefer subscriber.deinit();
 
-    const subscriber_1_thread = try std.Thread.spawn(
-        .{},
-        Subscriber(usize).run,
-        .{ &subscriber_1, &subscriber_1_ready_channel, &subscriber_1_close_channel },
-    );
-    subscriber_1_thread.detach();
-    const subscriber_2_thread = try std.Thread.spawn(
-        .{},
-        Subscriber(usize).run,
-        .{ &subscriber_2, &subscriber_2_ready_channel, &subscriber_2_close_channel },
-    );
-    subscriber_2_thread.detach();
+        try subscribers.append(subscriber);
+    }
 
-    const subscriber_3_thread = try std.Thread.spawn(
-        .{},
-        Subscriber(usize).run,
-        .{ &subscriber_3, &subscriber_3_ready_channel, &subscriber_3_close_channel },
-    );
-    subscriber_3_thread.detach();
+    for (publishers.items) |publisher| {
+        var ready_channel = UnbufferedChannel(bool).new();
 
-    const publisher_1_thread = try std.Thread.spawn(
-        .{},
-        Publisher(usize).run,
-        .{ &publisher_1, &publisher_1_ready_channel, &publisher_1_close_channel },
-    );
-    publisher_1_thread.detach();
-    const publisher_2_thread = try std.Thread.spawn(
-        .{},
-        Publisher(usize).run,
-        .{ &publisher_2, &publisher_2_ready_channel, &publisher_2_close_channel },
-    );
-    publisher_2_thread.detach();
+        const th = try std.Thread.spawn(
+            .{},
+            Publisher(VALUE_TYPE).run,
+            .{ publisher, &ready_channel },
+        );
+        th.detach();
 
-    _ = subscriber_1_ready_channel.receive();
-    _ = subscriber_2_ready_channel.receive();
-    _ = subscriber_3_ready_channel.receive();
-    _ = publisher_1_ready_channel.receive();
-    _ = publisher_2_ready_channel.receive();
+        _ = ready_channel.receive();
+    }
+
+    for (subscribers.items) |subscriber| {
+        var ready_channel = UnbufferedChannel(bool).new();
+
+        const th = try std.Thread.spawn(
+            .{},
+            Subscriber(VALUE_TYPE).run,
+            .{ subscriber, &ready_channel },
+        );
+        th.detach();
+
+        _ = ready_channel.receive();
+    }
 
     var timer = try std.time.Timer.start();
     const start = timer.read();
 
     for (0..ITERATIONS) |_| {
-        publisher_1.publish(1) catch {
-            log.err("publisher {} dropping", .{publisher_1.id});
-            std.time.sleep(1 * std.time.ns_per_ms);
-            continue;
-        };
-        publisher_2.publish(2) catch {
-            log.err("publisher {} dropping", .{publisher_2.id});
-            std.time.sleep(1 * std.time.ns_per_ms);
-            continue;
-        };
+        for (publishers.items) |publisher| {
+            try publisher.publish(publisher.id);
+        }
     }
 
-    const published_count = publisher_1.published_count + publisher_2.published_count;
-    var processed_count = subscriber_1.processed_count + subscriber_2.processed_count + subscriber_3.processed_count;
+    var published_count: u128 = 0;
 
+    while (published_count != ITERATIONS * PUBLISHER_COUNT) {
+        std.time.sleep(1 * std.time.ns_per_ms);
+        published_count = 0;
+        for (publishers.items) |publisher| {
+            published_count += publisher.published_count;
+        }
+    }
+
+    var processed_count: u128 = 0;
     while (processed_count != published_count) {
         std.time.sleep(1 * std.time.ns_per_ms);
-        processed_count = subscriber_1.processed_count + subscriber_2.processed_count + subscriber_3.processed_count;
+        processed_count = 0;
+        for (subscribers.items) |subscriber| {
+            processed_count += subscriber.processed_count;
+        }
     }
 
     log.err("took {}ms, total iters {}, total processed {}, total published {}", .{
@@ -279,7 +303,7 @@ pub fn main() !void {
         published_count,
     });
 
-    const expected_processed = 2 * ITERATIONS;
+    const expected_processed = ITERATIONS * PUBLISHER_COUNT;
 
     log.err("expected processed {}, actual processed {}, difference {}", .{
         expected_processed,
@@ -287,12 +311,13 @@ pub fn main() !void {
         expected_processed - processed_count,
     });
 
-    publisher_1_close_channel.send(true);
-    publisher_2_close_channel.send(true);
+    for (publishers.items) |publisher| {
+        publisher.close();
+    }
 
-    subscriber_1_close_channel.send(true);
-    subscriber_2_close_channel.send(true);
-    subscriber_3_close_channel.send(true);
+    for (subscribers.items) |subscriber| {
+        subscriber.close();
+    }
 
     assert(processed_count == published_count);
 
