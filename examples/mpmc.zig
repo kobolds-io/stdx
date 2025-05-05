eb27769a7ab8fb298a1f48d6460696ecb5bcbf0b
bgh <bengarishodge@pm.me>
2025-05-04 21:53:57 -0500

    rename mpmc types to producer/worker
    The example I wrote is more like a worker pattern which is very
    advantageous to many folks but it isn't an actual publisher/subscriber
    as I intended.

--- examples/mpmc.zig
+++ examples/mpmc.zig
@@ -10,21 +10,21 @@ const log = std.log.scoped(.MPSCExample);
 
 // This is the type that will be processed
 const VALUE_TYPE: type = usize;
-const TOPIC_QUEUE_SIZE = 50_000;
-const PUBLISHER_QUEUE_SIZE = 5_000;
-const SUBSCRIBER_QUEUE_SIZE = 5_000;
+const TOPIC_QUEUE_SIZE = 10_000;
+const PRODUCER_QUEUE_SIZE = 5_000;
+const WORKER_QUEUE_SIZE = 5_000;
 const ITERATIONS = 100_000;
-const SUBSCRIBER_COUNT = 5;
-const PUBLISHER_COUNT = 3;
-const PUBLISHER_BACKPRESSURE_MAX_CAPACITY = PUBLISHER_QUEUE_SIZE * 10;
+const WORKER_COUNT = 10;
+const PRODUCER_COUNT = 10;
+const PRODUCER_BACKPRESSURE_MAX_CAPACITY = PRODUCER_QUEUE_SIZE * 10;
 
 pub fn Topic(comptime T: type) type {
     return struct {
         const Self = @This();
 
         allocator: std.mem.Allocator,
-        queue: *RingBuffer(T),
         mutex: std.Thread.Mutex,
+        queue: *RingBuffer(T),
 
         pub fn init(allocator: std.mem.Allocator) !Self {
             const queue = try allocator.create(RingBuffer(T));
@@ -35,8 +35,8 @@ pub fn Topic(comptime T: type) type {
 
             return Self{
                 .allocator = allocator,
-                .queue = queue,
                 .mutex = std.Thread.Mutex{},
+                .queue = queue,
             };
         }
 
@@ -48,34 +48,35 @@ pub fn Topic(comptime T: type) type {
     };
 }
 
-pub fn Publisher(comptime T: type) type {
+pub fn Producer(comptime T: type) type {
     return struct {
         const Self = @This();
-        id: usize,
-        topic: *Topic(T),
+
         allocator: std.mem.Allocator,
-        queue: *RingBuffer(T),
-        published_count: u128,
-        mutex: std.Thread.Mutex,
-        close_channel: UnbufferedChannel(bool),
         backpressure: std.ArrayList(VALUE_TYPE),
+        close_channel: UnbufferedChannel(bool),
+        id: usize,
+        mutex: std.Thread.Mutex,
+        produced_count: u128,
+        queue: *RingBuffer(T),
+        topic: *Topic(T),
 
         pub fn init(allocator: std.mem.Allocator, id: usize, topic: *Topic(T)) !Self {
             const queue = try allocator.create(RingBuffer(T));
             errdefer allocator.destroy(queue);
 
-            queue.* = try RingBuffer(T).init(allocator, PUBLISHER_QUEUE_SIZE);
+            queue.* = try RingBuffer(T).init(allocator, PRODUCER_QUEUE_SIZE);
             errdefer queue.deinit();
 
             return Self{
-                .id = id,
-                .topic = topic,
-                .queue = queue,
                 .allocator = allocator,
-                .published_count = 0,
-                .mutex = .{},
-                .close_channel = UnbufferedChannel(bool).new(),
                 .backpressure = std.ArrayList(VALUE_TYPE).init(allocator),
+                .close_channel = UnbufferedChannel(bool).new(),
+                .id = id,
+                .mutex = .{},
+                .produced_count = 0,
+                .queue = queue,
+                .topic = topic,
             };
         }
 
@@ -85,24 +86,24 @@ pub fn Publisher(comptime T: type) type {
             self.allocator.destroy(self.queue);
         }
 
-        pub fn publish(self: *Self, value: T) !void {
+        pub fn produce(self: *Self, value: T) !void {
             self.mutex.lock();
             defer self.mutex.unlock();
 
             self.queue.enqueue(value) catch {
-                if (self.backpressure.items.len + 1 == PUBLISHER_BACKPRESSURE_MAX_CAPACITY) {
-                    log.err("Publisher: {} PUBLISHER_BACKPRESSURE_MAX_CAPACITY reached: {}", .{
+                if (self.backpressure.items.len + 1 == PRODUCER_BACKPRESSURE_MAX_CAPACITY) {
+                    log.err("producer: {} PRODUCER_BACKPRESSURE_MAX_CAPACITY reached: {}", .{
                         self.id,
-                        PUBLISHER_BACKPRESSURE_MAX_CAPACITY,
+                        PRODUCER_BACKPRESSURE_MAX_CAPACITY,
                     });
                     return error.BackpressureMaxCapacity;
                 }
                 try self.backpressure.append(value);
-                // log.err("publisher: {} adding value to backpressure", .{self.id});
+                // log.err("producer: {} adding value to backpressure", .{self.id});
                 return;
             };
 
-            self.published_count += 1;
+            self.produced_count += 1;
         }
 
         pub fn tick(self: *Self) !void {
@@ -120,7 +121,7 @@ pub fn Publisher(comptime T: type) type {
 
                 std.mem.copyForwards(VALUE_TYPE, self.backpressure.items, self.backpressure.items[n..]);
                 self.backpressure.items.len -= n;
-                self.published_count += @intCast(n);
+                self.produced_count += @intCast(n);
             }
 
             self.topic.queue.concatenateAvailable(self.queue);
@@ -132,8 +133,8 @@ pub fn Publisher(comptime T: type) type {
                 // check if we have received a signale to close the topic
                 const signal = self.close_channel.timedReceive(0) catch false;
                 if (signal) {
-                    // log.info("signal received to stop publisher {}", .{self.id});
-                    // log.debug("publisher {}: published {} items", .{ self.id, self.published_count });
+                    // log.info("signal received to stop producer {}", .{self.id});
+                    // log.debug("producer {}: produced {} items", .{ self.id, self.produced_count });
                     return;
                 }
 
@@ -148,31 +149,31 @@ pub fn Publisher(comptime T: type) type {
     };
 }
 
-pub fn Subscriber(comptime T: type) type {
+pub fn Worker(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        queue: *RingBuffer(T),
-        topic: *Topic(T),
-        id: usize,
-        processed_count: u128,
         allocator: std.mem.Allocator,
         close_channel: UnbufferedChannel(bool),
+        id: usize,
+        processed_count: u128,
+        queue: *RingBuffer(T),
+        topic: *Topic(T),
 
         pub fn init(allocator: std.mem.Allocator, id: usize, topic: *Topic(T)) !Self {
             const queue = try allocator.create(RingBuffer(T));
             errdefer allocator.destroy(queue);
 
-            queue.* = try RingBuffer(T).init(allocator, SUBSCRIBER_QUEUE_SIZE);
+            queue.* = try RingBuffer(T).init(allocator, WORKER_QUEUE_SIZE);
             errdefer queue.deinit();
 
             return Self{
                 .allocator = allocator,
-                .topic = topic,
+                .close_channel = UnbufferedChannel(bool).new(),
                 .id = id,
                 .processed_count = 0,
                 .queue = queue,
-                .close_channel = UnbufferedChannel(bool).new(),
+                .topic = topic,
             };
         }
 
@@ -197,8 +198,8 @@ pub fn Subscriber(comptime T: type) type {
                 // check if we have received a signale to close the topic
                 const signal = self.close_channel.timedReceive(0) catch false;
                 if (signal) {
-                    // log.err("signal received to stop subscriber {}", .{self.id});
-                    // log.err("subscriber {}: processed {} items", .{ self.id, self.processed_count });
+                    // log.err("signal received to stop worker {}", .{self.id});
+                    // log.err("worker {}: processed {} items", .{ self.id, self.processed_count });
                     return;
                 }
 
@@ -221,52 +222,52 @@ pub fn main() !void {
     var topic = try Topic(VALUE_TYPE).init(allocator);
     defer topic.deinit();
 
-    var publishers = std.ArrayList(*Publisher(VALUE_TYPE)).init(allocator);
-    defer publishers.deinit();
+    var producers = std.ArrayList(*Producer(VALUE_TYPE)).init(allocator);
+    defer producers.deinit();
 
-    var subscribers = std.ArrayList(*Subscriber(VALUE_TYPE)).init(allocator);
-    defer subscribers.deinit();
+    var workers = std.ArrayList(*Worker(VALUE_TYPE)).init(allocator);
+    defer workers.deinit();
 
-    for (0..PUBLISHER_COUNT) |i| {
-        const publisher = try allocator.create(Publisher(VALUE_TYPE));
-        errdefer allocator.destroy(publisher);
+    for (0..PRODUCER_COUNT) |i| {
+        const producer = try allocator.create(Producer(VALUE_TYPE));
+        errdefer allocator.destroy(producer);
 
-        publisher.* = try Publisher(VALUE_TYPE).init(allocator, i, &topic);
-        errdefer publisher.deinit();
+        producer.* = try Producer(VALUE_TYPE).init(allocator, i, &topic);
+        errdefer producer.deinit();
 
-        try publishers.append(publisher);
+        try producers.append(producer);
     }
 
-    for (0..SUBSCRIBER_COUNT) |i| {
-        const subscriber = try allocator.create(Subscriber(VALUE_TYPE));
-        errdefer allocator.destroy(subscriber);
+    for (0..WORKER_COUNT) |i| {
+        const worker = try allocator.create(Worker(VALUE_TYPE));
+        errdefer allocator.destroy(worker);
 
-        subscriber.* = try Subscriber(VALUE_TYPE).init(allocator, i, &topic);
-        errdefer subscriber.deinit();
+        worker.* = try Worker(VALUE_TYPE).init(allocator, i, &topic);
+        errdefer worker.deinit();
 
-        try subscribers.append(subscriber);
+        try workers.append(worker);
     }
 
-    for (publishers.items) |publisher| {
+    for (producers.items) |producer| {
         var ready_channel = UnbufferedChannel(bool).new();
 
         const th = try std.Thread.spawn(
             .{},
-            Publisher(VALUE_TYPE).run,
-            .{ publisher, &ready_channel },
+            Producer(VALUE_TYPE).run,
+            .{ producer, &ready_channel },
         );
         th.detach();
 
         _ = ready_channel.receive();
     }
 
-    for (subscribers.items) |subscriber| {
+    for (workers.items) |worker| {
         var ready_channel = UnbufferedChannel(bool).new();
 
         const th = try std.Thread.spawn(
             .{},
-            Subscriber(VALUE_TYPE).run,
-            .{ subscriber, &ready_channel },
+            Worker(VALUE_TYPE).run,
+            .{ worker, &ready_channel },
         );
         th.detach();
 
@@ -277,42 +278,42 @@ pub fn main() !void {
     const start = timer.read();
 
     for (0..ITERATIONS) |_| {
-        for (publishers.items) |publisher| {
-            publisher.publish(publisher.id) catch {
-                log.err("publisher: {} throttling", .{publisher.id});
+        for (producers.items) |producer| {
+            producer.produce(producer.id) catch {
+                log.err("producer: {} throttling", .{producer.id});
                 std.time.sleep(100 * std.time.ns_per_ms);
-                try publisher.publish(publisher.id);
+                try producer.produce(producer.id);
             };
         }
     }
 
-    var published_count: u128 = 0;
+    var produced_count: u128 = 0;
 
-    while (published_count != ITERATIONS * PUBLISHER_COUNT) {
+    while (produced_count != ITERATIONS * PRODUCER_COUNT) {
         std.time.sleep(1 * std.time.ns_per_ms);
-        published_count = 0;
-        for (publishers.items) |publisher| {
-            published_count += publisher.published_count;
+        produced_count = 0;
+        for (producers.items) |producer| {
+            produced_count += producer.produced_count;
         }
     }
 
     var processed_count: u128 = 0;
-    while (processed_count != published_count) {
+    while (processed_count != produced_count) {
         std.time.sleep(1 * std.time.ns_per_ms);
         processed_count = 0;
-        for (subscribers.items) |subscriber| {
-            processed_count += subscriber.processed_count;
+        for (workers.items) |worker| {
+            processed_count += worker.processed_count;
         }
     }
 
-    log.err("took {}ms, total iters {}, total processed {}, total published {}", .{
+    log.err("took {}ms, total iters {}, total processed {}, total produced {}", .{
         (timer.read() - start) / std.time.ns_per_ms,
         ITERATIONS,
         processed_count,
-        published_count,
+        produced_count,
     });
 
-    const expected_processed = ITERATIONS * PUBLISHER_COUNT;
+    const expected_processed = ITERATIONS * PRODUCER_COUNT;
 
     log.err("expected processed {}, actual processed {}, difference {}", .{
         expected_processed,
@@ -320,17 +321,17 @@ pub fn main() !void {
         expected_processed - processed_count,
     });
 
-    for (publishers.items) |publisher| {
-        log.err("publisher {} published {}", .{ publisher.id, publisher.published_count });
-        publisher.close();
+    for (producers.items) |producer| {
+        log.err("producer {} produced {}", .{ producer.id, producer.produced_count });
+        producer.close();
     }
 
-    for (subscribers.items) |subscriber| {
-        log.err("subscriber {} processed {}", .{ subscriber.id, subscriber.processed_count });
-        subscriber.close();
+    for (workers.items) |worker| {
+        log.err("worker {} processed {}", .{ worker.id, worker.processed_count });
+        worker.close();
     }
 
-    assert(processed_count == published_count);
+    assert(processed_count == produced_count);
 
     std.time.sleep(100 * std.time.ns_per_ms);
 }
