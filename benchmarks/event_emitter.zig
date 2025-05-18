9a7c78228b1eebbfccd90231a94e14a767f10f10
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-05-18 12:45:36 -0500

    add benchmark for event emitter

--- benchmarks/event_emitter.zig
+++ benchmarks/event_emitter.zig
@@ -9,33 +9,34 @@ const testing = std.testing;
 const EventEmitter = stdx.EventEmitter;
 
 const TestEvent = enum {
-    data_received,
-    data_reset,
+    add,
+    sub,
 };
 
-// const TestContext = struct {
-// total_data_received: u128,
-// };
+const TestContext = struct {
+    const Self = @This();
+    data: i128 = 0,
 
-const dataResetCallback = struct {
-    fn callback(_: u128) void {
-        // context.total_data_received = 0;
-    }
-}.callback;
+    pub fn onAdd(self: *Self, event: TestEvent, event_data: i128) void {
+        assert(event == .add);
 
-const dataReceivedCallback = struct {
-    fn callback(_: u128) void {
-        // context.total_data_received += @intCast(data);
+        self.data += event_data;
     }
-}.callback;
+
+    pub fn onSub(self: *Self, event: TestEvent, event_data: i128) void {
+        assert(event == .sub);
+
+        self.data -= event_data;
+    }
+};
 
 const BenchmarkEventEmitterEmit = struct {
     const Self = @This();
 
     list: *std.ArrayList(usize),
-    ee: *EventEmitter(TestEvent, u128),
+    ee: *EventEmitter(TestEvent, *TestContext, i128),
 
-    fn new(list: *std.ArrayList(usize), ee: *EventEmitter(TestEvent, u128)) Self {
+    fn new(list: *std.ArrayList(usize), ee: *EventEmitter(TestEvent, *TestContext, i128)) Self {
         return .{
             .list = list,
             .ee = ee,
@@ -43,20 +44,39 @@ const BenchmarkEventEmitterEmit = struct {
     }
 
     pub fn run(self: Self, _: std.mem.Allocator) void {
-        // defer self.ee.emit(.data_reset, 0);
-
         for (self.list.items) |data| {
-            self.ee.emit(.data_received, data);
+            self.ee.emit(.add, data);
         }
     }
 };
 
-var event_emitter_emit: EventEmitter(TestEvent, u128) = undefined;
+var event_emitter_emit_1: EventEmitter(TestEvent, *TestContext, i128) = undefined;
+var test_context_emit_1 = TestContext{};
+
+var event_emitter_emit_10: EventEmitter(TestEvent, *TestContext, i128) = undefined;
+var test_contexts_emit_10_list: std.ArrayList(*TestContext) = undefined;
+
+var event_emitter_emit_100: EventEmitter(TestEvent, *TestContext, i128) = undefined;
+var test_contexts_emit_100_list: std.ArrayList(*TestContext) = undefined;
 
 var data_list: std.ArrayList(usize) = undefined;
 const allocator = testing.allocator;
 
-fn afterEach() void {}
+fn afterEachEmit1() void {
+    test_context_emit_1.data = 0;
+}
+
+fn afterEachEmit10() void {
+    for (test_contexts_emit_10_list.items) |test_context| {
+        test_context.data = 0;
+    }
+}
+
+fn afterEachEmit100() void {
+    for (test_contexts_emit_100_list.items) |test_context| {
+        test_context.data = 0;
+    }
+}
 
 test "EventEmitter benchmarks" {
     var bench = zbench.Benchmark.init(
@@ -78,26 +98,111 @@ test "EventEmitter benchmarks" {
         data_list.appendAssumeCapacity(@intCast(i));
     }
 
-    const event_emitter_emit_title = try std.fmt.allocPrint(
+    // setup event emitter emit to 1 listener test
+    const event_emitter_emit_1_title = try std.fmt.allocPrint(
         allocator,
-        "emit {} items",
+        "emit 1 listeners {} items",
         .{constants.benchmark_max_queue_data_list},
     );
-    defer allocator.free(event_emitter_emit_title);
+    defer allocator.free(event_emitter_emit_1_title);
 
-    event_emitter_emit = EventEmitter(TestEvent, u128).init(allocator);
-    defer event_emitter_emit.deinit();
+    event_emitter_emit_1 = EventEmitter(TestEvent, *TestContext, i128).init(allocator);
+    defer event_emitter_emit_1.deinit();
 
-    try event_emitter_emit.addEventListener(.data_received, dataReceivedCallback);
-    defer _ = event_emitter_emit.removeEventListener(.data_received, dataReceivedCallback);
+    try event_emitter_emit_1.addEventListener(&test_context_emit_1, .add, TestContext.onAdd);
+    defer _ = event_emitter_emit_1.removeEventListener(&test_context_emit_1, .add, TestContext.onAdd);
+
+    // setup event emitter emit to 10 listeners test
+    const event_emitter_emit_10_title = try std.fmt.allocPrint(
+        allocator,
+        "emit 10 listeners {} items",
+        .{constants.benchmark_max_queue_data_list},
+    );
+    defer allocator.free(event_emitter_emit_10_title);
+
+    event_emitter_emit_10 = EventEmitter(TestEvent, *TestContext, i128).init(allocator);
+    defer event_emitter_emit_10.deinit();
+
+    test_contexts_emit_10_list = std.ArrayList(*TestContext).init(allocator);
+    defer test_contexts_emit_10_list.deinit();
+
+    for (0..10) |_| {
+        const test_context = try allocator.create(TestContext);
+        errdefer allocator.destroy(test_context);
+
+        test_context.* = TestContext{};
+
+        try test_contexts_emit_10_list.append(test_context);
+
+        try event_emitter_emit_10.addEventListener(test_context, .add, TestContext.onAdd);
+        errdefer _ = event_emitter_emit_10.removeEventListener(test_context, .add, TestContext.onAdd);
+    }
+
+    defer {
+        for (test_contexts_emit_10_list.items) |test_context| {
+            _ = event_emitter_emit_10.removeEventListener(test_context, .add, TestContext.onAdd);
+            allocator.destroy(test_context);
+        }
+    }
+
+    // setup event emitter emit to 100 listeners test
+    const event_emitter_emit_100_title = try std.fmt.allocPrint(
+        allocator,
+        "emit 100 listeners {} items",
+        .{constants.benchmark_max_queue_data_list},
+    );
+    defer allocator.free(event_emitter_emit_100_title);
+
+    event_emitter_emit_100 = EventEmitter(TestEvent, *TestContext, i128).init(allocator);
+    defer event_emitter_emit_100.deinit();
+
+    test_contexts_emit_100_list = std.ArrayList(*TestContext).init(allocator);
+    defer test_contexts_emit_100_list.deinit();
+
+    for (0..100) |_| {
+        const test_context = try allocator.create(TestContext);
+        errdefer allocator.destroy(test_context);
+
+        test_context.* = TestContext{};
+
+        try test_contexts_emit_100_list.append(test_context);
+
+        try event_emitter_emit_100.addEventListener(test_context, .add, TestContext.onAdd);
+        errdefer _ = event_emitter_emit_100.removeEventListener(test_context, .add, TestContext.onAdd);
+    }
+
+    defer {
+        for (test_contexts_emit_100_list.items) |test_context| {
+            _ = event_emitter_emit_100.removeEventListener(test_context, .add, TestContext.onAdd);
+            allocator.destroy(test_context);
+        }
+    }
 
     // register all the benchmark tests
     try bench.addParam(
-        event_emitter_emit_title,
-        &BenchmarkEventEmitterEmit.new(&data_list, &event_emitter_emit),
+        event_emitter_emit_1_title,
+        &BenchmarkEventEmitterEmit.new(&data_list, &event_emitter_emit_1),
         .{
             .hooks = .{
-                .after_each = afterEach,
+                .after_each = afterEachEmit1,
+            },
+        },
+    );
+    try bench.addParam(
+        event_emitter_emit_10_title,
+        &BenchmarkEventEmitterEmit.new(&data_list, &event_emitter_emit_10),
+        .{
+            .hooks = .{
+                .after_each = afterEachEmit10,
+            },
+        },
+    );
+    try bench.addParam(
+        event_emitter_emit_100_title,
+        &BenchmarkEventEmitterEmit.new(&data_list, &event_emitter_emit_100),
+        .{
+            .hooks = .{
+                .after_each = afterEachEmit100,
             },
         },
     );
