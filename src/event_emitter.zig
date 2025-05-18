1e1d29a6a312a2c3d5b2311755507595772240ac
bgh <bengarishodge@pm.me>
2025-05-18 12:46:53 -0500

    remove and rename dead code

--- src/event_emitter.zig
+++ src/event_emitter.zig
@@ -1,11 +1,16 @@
 const std = @import("std");
 const testing = std.testing;
+const assert = std.debug.assert;
 
-pub fn EventEmitter(comptime Event: type, comptime Context: type, comptime Data: type) type {
+pub fn EventEmitter(
+    comptime Event: type,
+    comptime Context: type,
+    comptime Data: type,
+) type {
     return struct {
         const Self = @This();
 
-        pub const ListenerCallback = *const fn (event: Event, context: Context, data: Data) void;
+        pub const ListenerCallback = *const fn (context: Context, event: Event, data: Data) void;
 
         pub const Listener = struct {
             context: Context,
@@ -14,13 +19,11 @@ pub fn EventEmitter(comptime Event: type, comptime Context: type, comptime Data:
 
         allocator: std.mem.Allocator,
         listeners: std.AutoHashMap(Event, *std.ArrayList(Listener)),
-        // mutex: std.Thread.Mutex,
 
         pub fn init(allocator: std.mem.Allocator) Self {
             return Self{
                 .allocator = allocator,
                 .listeners = std.AutoHashMap(Event, *std.ArrayList(Listener)).init(allocator),
-                // .mutex = std.Thread.Mutex{},
             };
         }
 
@@ -37,10 +40,7 @@ pub fn EventEmitter(comptime Event: type, comptime Context: type, comptime Data:
             self.listeners.deinit();
         }
 
-        pub fn addEventListener(self: *Self, event: Event, context: Context, callback: ListenerCallback) !void {
-            // self.mutex.lock();
-            // defer self.mutex.unlock();
-
+        pub fn addEventListener(self: *Self, context: Context, event: Event, callback: ListenerCallback) !void {
             if (self.listeners.get(event)) |listeners_list| {
                 try listeners_list.append(.{ .context = context, .callback = callback });
             } else {
@@ -57,13 +57,10 @@ pub fn EventEmitter(comptime Event: type, comptime Context: type, comptime Data:
             }
         }
 
-        pub fn removeEventListener(self: *Self, event: Event, callback: ListenerCallback) bool {
-            // self.mutex.lock();
-            // defer self.mutex.unlock();
-
+        pub fn removeEventListener(self: *Self, context: Context, event: Event, callback: ListenerCallback) bool {
             if (self.listeners.get(event)) |listener_list| {
                 for (listener_list.items, 0..listener_list.items.len) |listener, i| {
-                    if (listener.callback == callback) {
+                    if (listener.callback == callback and listener.context == context) {
                         _ = listener_list.swapRemove(i);
                         return true;
                     }
@@ -74,12 +71,9 @@ pub fn EventEmitter(comptime Event: type, comptime Context: type, comptime Data:
         }
 
         pub fn emit(self: *Self, event: Event, data: Data) void {
-            // self.mutex.lock();
-            // defer self.mutex.unlock();
-
             if (self.listeners.get(event)) |listener_list| {
                 for (listener_list.items) |listener| {
-                    listener.callback(event, listener.context, data);
+                    listener.callback(listener.context, event, data);
                 }
             }
         }
@@ -87,196 +81,46 @@ pub fn EventEmitter(comptime Event: type, comptime Context: type, comptime Data:
 }
 
 const TestEvent = enum {
-    open,
-    close,
+    data_sent,
+    data_received,
 };
 
-var test_number: u32 = 0;
-
-test "emits events to all listeners" {
-    const allocator = testing.allocator;
-    var ee = EventEmitter(TestEvent, ?*anyopaque, u32).init(allocator);
-    defer ee.deinit();
-
-    const callback1 = struct {
-        pub fn callback(event: TestEvent, context: ?*anyopaque, data: u32) void {
-            _ = event;
-            _ = context;
-            test_number += data;
-        }
-    }.callback;
-
-    const callback2 = struct {
-        pub fn callback(event: TestEvent, context: ?*anyopaque, data: u32) void {
-            _ = event;
-            _ = context;
-            test_number += data;
-        }
-    }.callback;
-
-    const callback3 = struct {
-        pub fn callback(event: TestEvent, context: ?*anyopaque, data: u32) void {
-            _ = event;
-            _ = context;
-            test_number += data;
-        }
-    }.callback;
-
-    try ee.addEventListener(.open, null, callback1);
-    try ee.addEventListener(.open, null, callback2);
-    try ee.addEventListener(.open, null, callback3);
-
-    ee.emit(.open, 1);
-
-    try testing.expectEqual(3, test_number);
-
-    try testing.expectEqual(true, ee.removeEventListener(.open, callback3));
-
-    ee.emit(.open, 10);
-
-    try testing.expectEqual(23, test_number);
-}
-
-const TestThreadEventEmitter = struct {
+const TestContext = struct {
     const Self = @This();
-    allocator: std.mem.Allocator,
-    ee: EventEmitter(TestEvent, ?*anyopaque, u32),
+    data: u32,
 
-    pub fn init(allocator: std.mem.Allocator) Self {
-        return Self{
-            .allocator = allocator,
-            .ee = EventEmitter(TestEvent, ?*anyopaque, u32).init(allocator),
-        };
-    }
+    pub fn onDataSent(self: *Self, event: TestEvent, data: u32) void {
+        assert(event == .data_sent);
 
-    pub fn deinit(self: *Self) void {
-        self.ee.deinit();
-    }
-
-    pub fn run(self: *Self) void {
-        var published_events: u32 = 0;
-        while (true) {
-            if (published_events == 100) {
-                return;
-            }
-
-            if (self.ee.listeners.count() > 0) {
-                published_events += 1;
-                self.ee.emit(.open, 1);
-            }
-        }
+        self.data = data;
     }
 };
 
-var test_number_2: u32 = 0;
-
-test "emits events over threads" {
+test "init/deinit" {
     const allocator = testing.allocator;
 
-    const callback1 = struct {
-        pub fn callback(event: TestEvent, context: ?*anyopaque, data: u32) void {
-            _ = event;
-            _ = context;
-            test_number_2 += data;
-        }
-    }.callback;
-
-    const callback2 = struct {
-        pub fn callback(event: TestEvent, context: ?*anyopaque, data: u32) void {
-            _ = event;
-            _ = context;
-            test_number_2 += data;
-        }
-    }.callback;
-
-    const callback3 = struct {
-        pub fn callback(event: TestEvent, context: ?*anyopaque, data: u32) void {
-            _ = event;
-            _ = context;
-            test_number_2 += data;
-        }
-    }.callback;
-
-    var t = TestThreadEventEmitter.init(allocator);
-    defer t.deinit();
-
-    try t.ee.addEventListener(.open, null, callback1);
-    try t.ee.addEventListener(.open, null, callback2);
-    try t.ee.addEventListener(.open, null, callback3);
-
-    const th = try std.Thread.spawn(.{}, TestThreadEventEmitter.run, .{&t});
-
-    th.join();
-
-    try testing.expectEqual(300, test_number_2);
+    var ee = EventEmitter(TestEvent, *TestContext, u32).init(allocator);
+    defer ee.deinit();
 }
 
-const Dog = struct {
-    name: []const u8,
-    age: i32,
-    happiness: i32,
-    energy: i32,
-};
-
-const DogEvent = enum {
-    birthday,
-    play,
-    eat,
-    sleep,
-};
-
-test "mutable context" {
+test "basic emit" {
     const allocator = testing.allocator;
-    var ee = EventEmitter(DogEvent, *Dog, i32).init(allocator);
+
+    var ee = EventEmitter(TestEvent, *TestContext, u32).init(allocator);
     defer ee.deinit();
 
-    const birthday = struct {
-        pub fn callback(_: DogEvent, context: *Dog, _: i32) void {
-            context.age += 1;
-        }
-    }.callback;
-
-    const play = struct {
-        pub fn callback(_: DogEvent, context: *Dog, data: i32) void {
-            context.happiness += data;
-            context.energy -= data * 10;
-        }
-    }.callback;
-
-    const eat = struct {
-        pub fn callback(_: DogEvent, context: *Dog, data: i32) void {
-            context.happiness += data;
-            context.energy += data * 5;
-        }
-    }.callback;
-
-    const sleep = struct {
-        pub fn callback(_: DogEvent, context: *Dog, data: i32) void {
-            context.energy += data * 10;
-        }
-    }.callback;
-
-    var context = Dog{
-        .name = "sardine",
-        .age = 0,
-        .happiness = 100,
-        .energy = 100,
+    var test_context = TestContext{
+        .data = 0,
     };
 
-    try ee.addEventListener(.birthday, &context, birthday);
-    try ee.addEventListener(.play, &context, play);
-    try ee.addEventListener(.eat, &context, eat);
-    try ee.addEventListener(.sleep, &context, sleep);
+    try ee.addEventListener(&test_context, .data_sent, TestContext.onDataSent);
+    defer _ = ee.removeEventListener(&test_context, .data_sent, TestContext.onDataSent);
 
-    // the story!
-    ee.emit(.sleep, 5);
-    ee.emit(.play, 7);
-    ee.emit(.birthday, 1);
-    ee.emit(.eat, 10);
-    ee.emit(.play, 40);
-    ee.emit(.sleep, 30);
+    const want: u32 = 100;
 
-    try testing.expectEqual(1, context.age);
-    try testing.expectEqual(157, context.happiness);
-    try testing.expectEqual(30, context.energy);
+    try testing.expectEqual(0, test_context.data);
+
+    ee.emit(.data_sent, 100);
+
+    try testing.expectEqual(want, test_context.data);
 }
