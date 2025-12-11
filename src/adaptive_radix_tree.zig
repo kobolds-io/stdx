58f826a66c21d09d254bb1d694ae473bd7bc8e77
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-12-10 22:57:39 -0600

    stub out core functions

--- src/adaptive_radix_tree.zig
+++ src/adaptive_radix_tree.zig
@@ -2,10 +2,13 @@ const std = @import("std");
 const testing = std.testing;
 
 pub fn AdaptiveRadixTree(comptime K: type, comptime V: type) type {
-    _ = K;
-    _ = V;
     return struct {
         const Self = @This();
+
+        const Node = struct {};
+
+        root: ?*Node = null,
+        size: usize = 0,
 
         pub fn init(allocator: std.mem.Allocator) Self {
             _ = allocator;
@@ -15,6 +18,25 @@ pub fn AdaptiveRadixTree(comptime K: type, comptime V: type) type {
         pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
             _ = self;
             _ = allocator;
+        }
+
+        pub fn lookup(self: *Self, allocator: std.mem.Allocator, key: K) ?*Node {
+            _ = self;
+            _ = allocator;
+            _ = key;
+        }
+
+        pub fn insert(self: *Self, allocator: std.mem.Allocator, key: K, value: V) !void {
+            _ = self;
+            _ = allocator;
+            _ = key;
+            _ = value;
+        }
+
+        pub fn delete(self: *Self, allocator: std.mem.Allocator, key: K) ?*Node {
+            _ = self;
+            _ = allocator;
+            _ = key;
         }
     };
 }
